09ace56d7ff6b154b45bad9cb26d46910a381a03
Chaz Schlarp <schlarpc@gmail.com>
2026-02-18 09:21:29 +0000

    Base flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+      ...
+    }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+      treefmtEval = eachSystem (
+        pkgs:
+        treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+        }
+      );
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = [
+            self.formatter.${pkgs.stdenv.hostPlatform.system}
+            pkgs.ponysay
+          ];
+
+          env = { };
+
+          shellHook = "";
+        };
+      });
+
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.stdenv.hostPlatform.system}.config.build.wrapper);
+
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.stdenv.hostPlatform.system}.config.build.check self;
+      });
+    };
+}
